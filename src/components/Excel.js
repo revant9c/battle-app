8a965c88130efcabbb25d5d5f9d281260c91b4a2
Revant Chandanala <rchandanala@nextgen.com>
2018-03-29 14:58:40 -0700

    WIP: make table cells editable

--- src/components/Excel.js
+++ src/components/Excel.js
@@ -4,19 +4,59 @@ import PropTypes from 'prop-types';
 class Excel extends Component {
     constructor(props) {
         super(props);
-        this.state = {data: props.data};
+        this.state = {
+            data: props.data,
+            sortBy: null,
+            descending: false,
+            edit: null
+        };
         this.sortTableData = this.sortTableData.bind(this);
+        this.changeSortIcon = this.changeSortIcon.bind(this);
+        this.editTableData = this.editTableData.bind(this);
     }
 
+    /**
+     * Method to sort table data
+     * @param event
+     */
     sortTableData(event) {
         const columnName = event.target.cellIndex,
             data = Array.from(this.state.data);
+        let descending = this.state.sortBy === columnName && !this.state.descending;
 
-        data.sort((a, b) => a[columnName] > b[columnName] ? 1 : -1);
+        data.sort((a, b) => descending ? a[columnName] < b[columnName] ? 1 : -1 : a[columnName] > b[columnName] ? 1 : -1);
 
         this.setState({
-            data: data
+            data: data,
+            sortBy: columnName,
+            descending: descending,
         })
+    }
+
+    /**
+     * Method to edit table cell data
+     * @param event
+     */
+    editTableData(event) {
+        this.setState({
+            edit: {
+                row: parseInt(event.target.dataset.row, 10),
+                cell: event.target.cellIndex
+            }
+        })
+    }
+
+    /**
+     * Method to change the icon when table header is clicked
+     * @param title
+     * @param headerId
+     * @returns title
+     */
+    changeSortIcon(title, headerId) {
+        if (this.state.sortBy === headerId) {
+            title += this.state.descending ? '\u2193' : '\u2191'
+        }
+        return title;
     }
 
     render () {
@@ -25,17 +65,17 @@ class Excel extends Component {
         return (
             <div>
                 <table>
-                    <thead onClick={this.sortTableData}>
+                    <thead onDoubleClick={this.sortTableData}>
                         <tr>
-                            {headers.map((title, id) => <th key={id}>{title}</th>)}
+                            {headers.map((title, headerId) => <th key={headerId}>{this.changeSortIcon(title, headerId)}</th> )}
                         </tr>
                     </thead>
-                    <tbody>
-                    {this.state.data.map((tRow, id) =>
-                        <tr key={id}>
-                            {tRow.map((tData, id) => <td key={id}>{tData}</td>)}
-                        </tr>
-                    )}
+                    <tbody onClick={this.editTableData}>
+                        {this.state.data.map((tRow, rowId) =>
+                            <tr key={rowId}>
+                                {tRow.map((tData, tdId) => <td data-row={rowId} key={tdId}>{tData}</td>)}
+                            </tr>
+                        )}
                     </tbody>
                 </table>
             </div>
