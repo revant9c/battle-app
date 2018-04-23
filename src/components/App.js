963db2e78d53607cdf277819b2c4cecfba2e8f38
revant <revant@reflexionsdata.com>
2018-04-22 22:31:58 -0700

    minor change

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Popular from './Popular';
+import { BrowserRouter, Route} from 'react-router-dom';
+import Nav from './nav';
+import Home from './home';
+
+
+class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <div className='container'>
+                    <Nav />
+                    <Route exact path="/" component={Home} />
+                    <Route path="/popular" component={Popular} />
+                </div>
+            </BrowserRouter>
+        );
+    }
+}
+
+export default App;
