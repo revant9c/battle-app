0e72bb4254b746157b238e71b43e8e7ea97f3b85
revant <revant@reflexionsdata.com>
2018-04-22 22:21:59 -0700

    finished popular component and added nav bar with the help of reacr router

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import Popular from './Popular';
+import { BrowserRouter, Route} from 'react-router-dom';
+import Nav from './nav';
+import Home from './home';
 
 
 class App extends Component {
     render() {
         return (
-            <div className='container'>
-                <Popular />
-            </div>
+            <BrowserRouter>
+                <div className='container'>
+                    <Nav />
+                    <Route exact path="/" component={Home} />
+                    <Route path="/popular" component={Popular} />
+                </div>
+            </BrowserRouter>
         );
     }
 }
--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
+import { fetchPopularRepos } from '../utils/api';
 
 function SelectLanguage(props) {
-    let languages = ['All', 'JavaScript', 'ReactJs', 'Swift', 'PHP', 'CSS'];
+    let languages = ['All', 'JavaScript', 'Java', 'Swift', 'PHP', 'CSS'];
     return (
         <ul className='languages'>
             {languages.map(language =>
@@ -22,26 +22,67 @@ SelectLanguage.propTypes = {
     onSelect: PropTypes.func.isRequired
 };
 
+function RepoGrid(props) {
+    return (
+        <ul className='popular-list'>
+            {props.repos.map((repo, index) =>
+                <li key={repo.name} className="popular-item">
+                    <div className="popular-rank">#{index + 1}</div>
+                    <ul className="space-list-items">
+                        <li>
+                            <img className="avatar"
+                                 src={repo.owner.avatar_url}
+                                 alt={'Avatar for ' + repo.owner.login} />
+                        </li>
+                        <li><a href={repo.html_url}>{repo.name}</a></li>
+                        <li>@{repo.owner.login}</li>
+                        <li>{repo.stargazers_count} stars</li>
+                    </ul>
+                </li>
+            )}
+        </ul>
+    )
+}
+
+RepoGrid.propTypes = {
+    repos: PropTypes.array.isRequired
+};
+
 class Popular extends Component {
     constructor(props) {
         super(props);
         this.state = {
             selectedLanguage : 'All',
+            repos : null
         };
 
         this.updateLanguage = this.updateLanguage.bind(this);
     }
 
+    componentDidMount() {
+       this.updateLanguage(this.state.selectedLanguage);
+    }
+
     updateLanguage(lang) {
         this.setState(() => ({
-            selectedLanguage: lang
+            selectedLanguage: lang,
+            repos: null
         }));
+
+        fetchPopularRepos(lang)
+            .then(repos => {
+                this.setState(() => ({
+                    repos: repos
+                }));
+            });
     }
 
     render() {
         return (
             <div>
                 <SelectLanguage selectedLanguage={this.state.selectedLanguage} onSelect={this.updateLanguage}/>
+                {!this.state.repos ? <p>LOADING</p> :
+                <RepoGrid repos={this.state.repos} />}
             </div>
 
         );
